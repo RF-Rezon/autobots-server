58fbb77f93512ee38905ed110d3777fc14db7dca
RF-Rezon <rezwanfaysalrezon@gmail.com>
2023-05-20 01:37:34 +0600

    api

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 require('dotenv').config();
 var cors = require('cors')
@@ -9,7 +10,31 @@ app.use(cors())
 
 const port = process.env.PORT || 5000;
 
-console.log(process.env.PORT)
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@autobotcluster.vriex22.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch(err){
+    console.log("MongoDb connection problem.")
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
